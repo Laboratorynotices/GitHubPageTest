75a2a690c20e12bdb06164176836cea0235c242b
Laboratorynotices <muenchen.webas@gmail.com>
2023-08-20 22:52:52 +0200

    feat:

--- src/layouts/constrained/LayoutMain.vue
+++ src/layouts/constrained/LayoutMain.vue
@@ -1,36 +1,12 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
-import HelloWorld from '@/components/HelloWorld.vue'
-import { navElements } from "@/menu-elements";
+import { RouterView } from 'vue-router';
+import HelloWorld from '@/components/HelloWorld.vue';
+import NavBar from '@/layouts/constrained/NavBar.vue';
 </script>
 
 <template>
-
   <v-app id="inspire">
-    <v-app-bar flat>
-      <v-container class="fill-height d-flex align-center">
-        <v-avatar class="me-10 ms-4" color="grey-darken-1" size="32"></v-avatar>
-
-        <!-- Навигация из файла App.vue -->
-        <nav>
-          <v-btn
-            v-for="(navElement, i) in navElements"
-            :key="i"
-            :to="navElement.path"
-          >{{ navElement.nameMenu }}</v-btn>
-        </nav>
-
-        <v-spacer></v-spacer>
-
-        <v-responsive max-width="260">
-          <v-text-field
-            density="compact"
-            hide-details
-            variant="solo"
-          ></v-text-field>
-        </v-responsive>
-      </v-container>
-    </v-app-bar>
+    <nav-bar></nav-bar>
 
     <v-main class="bg-grey-lighten-3">
       <v-container>
---
+++ src/layouts/constrained/NavBar.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { navElements } from "@/menu-elements";
+</script>
+
+<template>
+  <v-app-bar flat>
+    <v-container class="fill-height d-flex align-center">
+      <v-avatar class="me-10 ms-4" color="grey-darken-1" size="32"></v-avatar>
+
+      <!-- Навигация из файла App.vue -->
+      <nav>
+        <v-btn
+          v-for="(navElement, i) in navElements"
+          :key="i"
+          :to="navElement.path"
+        >{{ navElement.nameMenu }}</v-btn>
+      </nav>
+
+      <v-spacer></v-spacer>
+
+      <v-responsive max-width="260">
+        <v-text-field
+          density="compact"
+          hide-details
+          variant="solo"
+        ></v-text-field>
+      </v-responsive>
+    </v-container>
+  </v-app-bar>
+</template>
