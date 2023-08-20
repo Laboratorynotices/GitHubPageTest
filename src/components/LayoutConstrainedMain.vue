c8fd027a5b8ad636e8009aad4582c7e97ac9aa71
Laboratorynotices <muenchen.webas@gmail.com>
2023-08-21 00:27:37 +0200

    feat:

--- src/components/LayoutConstrainedMain.vue
+++ src/components/LayoutConstrainedMain.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
-import DummyComponent from '@/components/DummyComponent.vue';
 import NavBar from '@/components/LayoutsConstrainedNavBar.vue';
 </script>
 
@@ -39,6 +38,4 @@ import NavBar from '@/components/LayoutsConstrainedNavBar.vue';
   </v-app>
 
   <v-divider class="border-opacity-100 mt-6" color="success"></v-divider>
-
-  <dummy-component />
 </template>
