859040d6f609ae1ba568c1cd56c24adcdf32c5c3
Laboratorynotices <muenchen.webas@gmail.com>
2023-08-20 22:26:15 +0200

    feat:

--- src/layouts/constrained/LayoutMain.vue
+++ src/layouts/constrained/LayoutMain.vue
@@ -71,8 +71,6 @@ const links = ["Dashboard", "Messages", "Profile", "Updates"];
   <v-divider class="border-opacity-100" color="success"></v-divider>
 
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
