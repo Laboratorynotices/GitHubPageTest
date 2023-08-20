f4e7d6d79a4def7b396f2516c61d6a453280dc4b
Laboratorynotices <muenchen.webas@gmail.com>
2023-08-20 21:29:24 +0200

    feat:

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory, type NavigationGuardNext, type RouteLocationNormalized } from 'vue-router';
 import { routes } from '@/menu-elements';
 //import HomeView from '../views/HomeView.vue'
 
@@ -22,6 +22,21 @@ const router = createRouter({
     }
   ]
 */
-})
+});
+
+/**
+ * При помощи JS обновляем мета-заголовок страницы.
+ */
+router.beforeEach(
+  (
+    to: RouteLocationNormalized,
+    from: RouteLocationNormalized,
+    next: NavigationGuardNext
+  ): void => {
+    document.title =
+      (to.meta.title ? to.meta.title + " -- " : "") + "Vue.js 3 and Firebase";
+    next();
+  }
+);
 
 export default router
